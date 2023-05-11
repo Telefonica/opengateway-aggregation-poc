5955e6c237fbba4ee8780dd2bb978affdfd432d1
Juan Hernando <juanantonio.hernandolabajo@telefonica.com>
2023-05-11 11:39:53 +0200

    add authserver and telco-router dockers

---
+++ mongo-seed/routerApp.js
@@ -0,0 +1,29 @@
+db.apps.replaceOne(
+    {
+        _id : "73902489-c201-4819-bdf9-3708a484fe21"
+    },
+    {
+        _id : "73902489-c201-4819-bdf9-3708a484fe21",
+        consumer_secret : "3184428a-1ea4-4e1c-9969-b623f36fbc2f",
+        name : "Demo App",
+        description : "App for testing",
+        developer : {
+            email : "johndoe@demoapp.com",
+            name : "John Doe"
+        },
+        status : "active",
+        grants : [
+            {
+                grant_type : "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                scopes : [
+                    "device-location-verification-verify-read"
+                ]
+            }
+        ],
+        sector_identifier_uri : "http://localhost:3000",
+        jwks_uri : "http://demo-app:3000/api/jwks"
+    },
+    {
+        upsert: true
+    }
+)
--- operator-platform/authserver/docker-entrypoint.sh
+++ operator-platform/authserver/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-APPNAME=baikal-aggregator
+APPNAME=baikal-authserver
 USER=baikal
-APPMODULE=aggregator.wsgi:application
+APPMODULE=authserver.wsgi:application
 DAEMON=gunicorn
 BIND=0.0.0.0:${PORT}
 [ -z "${WORKERS}" ] && WORKERS=2 || true
-BASE_DIR="/opt/baikal/baikal-aggregator"
+BASE_DIR="/opt/baikal/baikal-authserver"
 
 pushd ${BASE_DIR} && ${DAEMON} --bind=${BIND} --workers=${WORKERS} ${APPMODULE}
