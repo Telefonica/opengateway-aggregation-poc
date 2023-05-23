4386d53b5083497a0315655753129beb34b1a2d9
garciasolero <juanfabio.garciasolero@telefonica.com>
2023-05-23 09:47:06 +0200

    Add view scope requirements by method

--- operator-platform/authserver/service/src/authserver/utils/views.py
+++ operator-platform/authserver/service/src/authserver/utils/views.py
@@ -1,5 +1,6 @@
 from django.utils.decorators import classonlymethod
 from django.views.generic.base import View
+from requests.structures import CaseInsensitiveDict
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.renderers import JSONRenderer
@@ -23,10 +24,16 @@ class GrantScopePermissions(BasePermission):
 
     def has_permission(self, request, view):
         if request.user and request.user.is_authenticated:
+            required_scopes = self._get_view_scopes(request, view)
             for grant in request.user.data[ApplicationCollection.FIELD_GRANTS]:
-                if grant[Grant.FIELD_GRANT_TYPE] in view.grant_types and len(set(grant[Grant.FIELD_SCOPES]) & set(view.scopes)):
+                if grant[Grant.FIELD_GRANT_TYPE] in view.grant_types and len(set(grant[Grant.FIELD_SCOPES]) & set(required_scopes)):
                     return True
         return False
+
+    def _get_view_scopes(self, request, view):
+        if isinstance(view.scopes, CaseInsensitiveDict):
+            return view.scopes.get(request.method, [])
+        return view.scopes or []
 
 
 class JSONBasicAuthenticatedView(APIView):
@@ -39,6 +46,12 @@ class JSONBasicAuthenticatedView(APIView):
 
     scopes = []
     grant_types = ['basic']
+
+    def __new__(cls, *args, **kwargs):
+        if cls.scopes is not None and isinstance(cls.scopes, dict):
+            cls.scopes = CaseInsensitiveDict(cls.scopes)
+
+        return super().__new__(cls)
 
 
 def publish_to_middleware(**kwargs):
