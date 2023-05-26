92948a6955bb217eb32d73c3b28a1512e92a2da3
Guido García <guido.garciabernardo@telefonica.com>
2023-05-26 09:40:21 +0200

    chore: configure azure envs (wip)

--- mongo-seed/docker-entrypoint.sh
+++ mongo-seed/docker-entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-mongo mongodb:27017/authserver-telefonica --quiet app-provision.js
+url="http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+http_code=$(curl  -H "Metadata: true" -s -o /dev/null -w "%{http_code}" "$url")
 
-mongo mongodb:27017/authserver-vodafone --quiet app-provision.js
-
-mongo mongodb:27017/aggregator-telco-router-1 --quiet aggregator-app-provision.js
-
-mongo mongodb:27017/aggregator-telco-router-2 --quiet aggregator-app-provision.js
+if [ "$http_code" -eq 200 ]; then
+    # This code is Telefónica specific, for our deployment in Azure.
+    # It is not needed for local development.
+    mongo mongodb:27017/authserver-telefonica --quiet azure/app-provision.js
+    mongo mongodb:27017/authserver-vodafone --quiet azure/app-provision.js
+    mongo mongodb:27017/aggregator-telco-router-1 --quiet azure/aggregator-app-provision.js
+    mongo mongodb:27017/aggregator-telco-router-2 --quiet azure/aggregator-app-provision.js
+else
+    mongo mongodb:27017/authserver-telefonica --quiet localhost/app-provision.js
+    mongo mongodb:27017/authserver-vodafone --quiet localhost/app-provision.js
+    mongo mongodb:27017/aggregator-telco-router-1 --quiet localhost/aggregator-app-provision.js
+    mongo mongodb:27017/aggregator-telco-router-2 --quiet localhost/aggregator-app-provision.js
+fi
