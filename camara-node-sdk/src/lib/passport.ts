6c2b00a895c5d079a3d3c4a8f11fe3b37d41f553
Juan Hernando <juanantonio.hernandolabajo@telefonica.com>
2023-05-27 07:51:03 +0200

    fix: remove check

--- camara-node-sdk/src/lib/passport.ts
+++ camara-node-sdk/src/lib/passport.ts
@@ -19,9 +19,6 @@ const retrieveParametersFromRequest = (req: any) => {
   const state: string = (req.query.state ?? '') as string;
   const scope: string = req.session?.scope as string;
   let error = '';
-  if (!scope) {
-    error = 'No scope stablished. Performing logout';
-  }
 
   if (!req.session?.login || !req.session.login.phonenumber) {
     error = "No phone number found. Performing logout";
